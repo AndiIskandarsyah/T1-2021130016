e122d479fe65e580c88bf1bb787fc537d29f8f3e
AndiIskandarsyah <andiiskandarsyah923@gmail.com>
2023-10-16 08:49:57 +0700

    edit landing dan template

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,45 +1,40 @@
 @extends('layouts.template')
 
-@section('title', 'Landing Page')
+@section('title', 'Tentang Perpustakaan') <!-- Mengganti judul halaman -->
 
 @section('content')
+    <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+        <div class="row mb-2">
+            <div class="col-md-6 px-0">
+                <h1 class="display-4 fst-italic">Selamat Datang di Perpustakaan</h1> <!-- Menambah judul selamat datang -->
+                <p class="lead my-3">Kami menyediakan akses ke beragam koleksi buku dan sumber daya bermanfaat.</p> <!-- Tambahkan deskripsi perpustakaan -->
+                <p class="lead mb-0">
+                    <a href="#" class="text-white fw-bold">
+                        Telusuri lebih lanjut...
+                    </a>
+                </p>
+            </div>
+            <div class="col-md-6">
+                <!-- Isi gambar atau konten visual yang relevan tentang perpustakaan -->
+            </div>
+        </div>
+    </div>
 
-        <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
-            <div class="row mb-2">
-                <div class="col-md-6 px-0">
-                    <h1 class="display-4 fst-italic"></h1>
-                    <p class="lead my-3"></p>
-                    <p class="lead mb-0">
-                        <a href="" class="text-white fw-bold">
-                            Continue reading...
-                        </a>
-                    </p>
+    {{-- Artikel Perpustakaan --}}
+    <div class="row mb-2">
+        <div class="col-md-6">
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                    <strong class="d-inline-block mb-2 text-primary">Berita Terbaru</strong> <!-- Ganti kategori "World" dengan "Berita Terbaru" -->
+                    <h3 class="mb-0">Judul Artikel Terbaru</h3> <!-- Ganti judul artikel -->
+                    <div class="mb-1 text-muted">Tanggal Terbit</div> <!-- Ganti tanggal terbit -->
+                    <p class="card-text mb-auto">Deskripsi artikel singkat.</p> <!-- Ganti deskripsi artikel -->
+                    <a href="#" class="stretched-link">Baca selengkapnya</a> <!-- Ganti link "Continue reading" dengan "Baca selengkapnya" -->
                 </div>
-                <div class="col-md-6">
+                <div class="col-auto d-none d-lg-block">
+                    <!-- Isi gambar artikel atau konten visual yang relevan -->
                 </div>
             </div>
         </div>
-
-
-    {{-- Articles Card --}}
-    <div class="row mb-2">
-
-            <div class="col-md-6">
-                <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                    <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-primary">World</strong>
-                        <h3 class="mb-0"></h3>
-                        <div class="mb-1 text-muted"></div>
-                        <p class="card-text mb-auto"></p>
-                        <a href="" class="stretched-link">Continue reading</a>
-                    </div>
-                    <div class="col-auto d-none d-lg-block">
-
-                    </div>
-                </div>
-            </div>
-
-
-        {{$books->links()}}
     </div>
 @endsection
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -14,10 +14,10 @@
         <header class="blog-header py-3">
             <div class="row flex-nowrap justify-content-between align-items-center">
                 <div class="col-4 pt-1">
-                    <a class="link-secondary" href="#">Subscribe</a>
+                    <a class="link-secondary" href="#">Daftar</a>
                 </div>
                 <div class="col-4 text-center">
-                    <a class="blog-header-logo text-dark" href="#">Large</a>
+                    <a class="blog-header-logo text-dark" href="#">T1-2021130016 Perpustakaan</a>
                 </div>
                 <div class="col-4 d-flex justify-content-end align-items-center">
                     <a class="link-secondary" href="#" aria-label="Search">
@@ -36,18 +36,11 @@
 
         <div class="nav-scroller py-1 mb-2">
             <nav class="nav d-flex justify-content-between">
-                <a class="p-2 link-secondary" href="#">World</a>
-                <a class="p-2 link-secondary" href="#">U.S.</a>
-                <a class="p-2 link-secondary" href="#">Technology</a>
-                <a class="p-2 link-secondary" href="#">Design</a>
-                <a class="p-2 link-secondary" href="#">Culture</a>
-                <a class="p-2 link-secondary" href="#">Business</a>
-                <a class="p-2 link-secondary" href="#">Politics</a>
-                <a class="p-2 link-secondary" href="#">Opinion</a>
-                <a class="p-2 link-secondary" href="#">Science</a>
-                <a class="p-2 link-secondary" href="#">Health</a>
-                <a class="p-2 link-secondary" href="#">Style</a>
-                <a class="p-2 link-secondary" href="#">Travel</a>
+                <a class="p-2 link-secondary" href="#">Koleksi Buku</a>
+                <a class="p-2 link-secondary" href="#">Peminjaman</a>
+                <a class="p-2 link-secondary" href="#">Anggota</a>
+                <a class="p-2 link-secondary" href="#">Tentang Kami</a>
+                <a class="p-2 link-secondary" href="#">Kontak</a>
             </nav>
         </div>
     </div>
